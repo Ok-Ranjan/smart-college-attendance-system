0ad6d8038d960666ce855db9abcb6b25bbaf8754
Ok-Ranjan <ranjanmp802114@gmail.com>
2026-03-06 13:34:43 +0530

    Models: define Student & Teacher models in accountsapp

--- smart_attendance/accounts/models.py
+++ smart_attendance/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    roll_number = models.CharField(max_length=20, unique=True)
+    branch = models.CharField(max_length=50)
+    year = models.IntegerField()
+
+    character_score = models.FloatField(default=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.roll_number})"
+    
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
