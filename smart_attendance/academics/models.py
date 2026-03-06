1fe8f973356cfecf30bf7f925f38907e028f3b19
Ok-Ranjan <ranjanmp802114@gmail.com>
2026-03-06 13:49:53 +0530

    academics/Models: define Assignment & Activity models

--- smart_attendance/academics/models.py
+++ smart_attendance/academics/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from accounts.models import Student
 # Create your models here.
+
+class Assignment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+
+    marks = models.FloatField(default=0)
+
+    submitted = models.BooleanField(default=False)
+
+
+class Activity(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    activity_name = models.CharField(max_length=200)
+
+    points = models.FloatField(default=0)
+
