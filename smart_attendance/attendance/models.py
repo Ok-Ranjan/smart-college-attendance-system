c9ba6c94222efac993c7e538047c0a83c6a30515
Ok-Ranjan <ranjanmp802114@gmail.com>
2026-03-06 13:42:57 +0530

    Models: define Attendance models class in attendance app

--- smart_attendance/attendance/models.py
+++ smart_attendance/attendance/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from accounts.models import Student, Teacher
 # Create your models here.
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    date = models.DateField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.data}"
+    
+
